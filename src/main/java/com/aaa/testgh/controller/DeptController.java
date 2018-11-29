2d67d793cecbd308ac3442e21939a1c5347c87a6
aaaqy88fhm <2268162655@qq.com>
2018-11-29 14:47:45 +0800

    Merge branch 'dev'
    # Conflicts:
    #	src/main/java/com/aaa/testgh/controller/DeptController.java
    #	src/main/java/com/aaa/testgh/controller/EmpController.java

--- src/main/java/com/aaa/testgh/controller/DeptController.java
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -10,5 +10,6 @@ public class DeptController {
     public static void main(String[] args) {
         System.out.println("测试github提交1111...");
         System.out.println("测试合并分支。。。");
+        System.out.println("20181128");
     }
 }
