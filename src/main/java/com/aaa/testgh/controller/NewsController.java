81c2669be101b9621c7533125e78cd86b8190eb1
aaaqy88fhm <2268162655@qq.com>
2018-11-29 13:39:18 +0800

    新闻测试

---
+++ src/main/java/com/aaa/testgh/controller/NewsController.java
@@ -0,0 +1,14 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:NewsController
+ * discription:
+ * author:fhm
+ * createTime:2018-11-29 13:37
+ */
+public class NewsController {
+
+    public static void main(String[] args) {
+        System.out.println("新闻测试。。。");
+    }
+}
